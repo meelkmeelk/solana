8315ad19ea2333e496f666655194c186a1f0eaf4
meelkmeelk <teoheanyikofficial@gmail.com>
2023-11-16 18:23:41 +0800

    day2

---
+++ generate-keypair/generate-keypair.ts
@@ -0,0 +1,22 @@
+import * as dotenv from "dotenv";
+import { getKeypairFromEnvironment } from "@solana-developers/node-helpers";
+
+dotenv.config();
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
+
+
+// Creating the public and private key
+// import { Keypair } from "@solana/web3.js";
+
+// const keypair = Keypair.generate();
+
+// console.log(`The public key is: `, keypair.publicKey.toBase58());
+// console.log(`The secret key is: `, keypair.secretKey);
+// console.log(`✅ Finished!`);
+
+
